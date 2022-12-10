a1dab488b92e9943287e20783b4579f5392f8bdc
shubhamkanani <shubhamkanani605@gmail.com>
2022-12-09 23:20:04 -0500

    Final Project

---
+++ routes/event.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const eventController = require("../controller/event.controller");
+
+const router = express.Router();
+
+router.get("/", eventController.getAllEvent);
+router.post("/createEvent", eventController.createEvent);
+router.patch("/updateEvent/:id", eventController.updateEvent);
+router.delete("/deleteEvent/:id", eventController.deleteEvent);
+router.get("/getDailyEvent/:id", eventController.getDailyEvent);
+router.get("/getEvent/:id", eventController.getEventById);
+router.get("/getGoogleEvent", eventController.getGoogleEvent);
+router.get("/getString", eventController.getString);
+// router.patch("/updateDeadline/:endTime",eventController.updateDeadline)
+
+module.exports = router;
